98c5ee6c0dd5b375657892363f052ce6dbb08bac
Ludonovan <lucasdonovan@live.com>
2024-03-13 18:34:23 -0400

    7 bad. 8-10 ok. 11 bad. 12-14 ok. 15 bad

--- wish/exec.c
+++ wish/exec.c
@@ -14,7 +14,7 @@ extern char error_message[30];
 extern char *PATH[MAX_PATH];
 
 char *next_arg;
-
+int found = 0;
 void print_error(){ write(STDERR_FILENO, error_message, strlen(error_message)); }
 
 
@@ -40,13 +40,6 @@ void exec_cmd(char *PATH[MAX_PATH], char **exec_args) { // execute other command
 	}
     }
      
-    int found = 0;
-    for (int i = 0; i < MAX_PATH; i++) {
-        if (access(PATH[found], X_OK) != 0) { 
-	    found++;
-        }
-    }
-      
     if (execv(PATH[found], exec_args) != 0 || found == MAX_PATH) { 
         print_error();
         exit(1);
@@ -73,8 +66,9 @@ void exec_cd(char **exec_args) { // cd
 void exec_path(char **exec_args) { // path
     if (exec_args[1] != NULL) { // has args
         for (int j = 0; j < num_args - 1; j++) {
+	    //if (PATH[j] != NULL) free(PATH[j]);
 	    PATH[j] = malloc(strlen(exec_args[j+1]) + 3);
-	    char *tmp = malloc(strlen("/" + 30));
+	    char *tmp = malloc(strlen(exec_args[j+1]) + 3);
 	    strcpy(tmp, "/");
 	    strcat(tmp, exec_args[1]);
 	    strcpy(PATH[j], tmp);
@@ -82,7 +76,8 @@ void exec_path(char **exec_args) { // path
 	    free(tmp);
 	}
     } else if (exec_args[1] == NULL) { // no args
-        for (int i = 0; i < MAX_PATH - 1; i++) { 
+        for (int i = 0; i < MAX_PATH - 1; i++) {
+	    if (PATH[i] != NULL) free(PATH[i]);	
             PATH[i] = NULL;
         }
     }
@@ -100,7 +95,18 @@ void exec_exit(char **exec_args) { // exit
 }
 
 void exec(char **exec_args) {
-    
+    for (int i = 0; i < MAX_PATH; i++) {
+	if (PATH[i] != NULL && access(PATH[i], X_OK) == 0) {
+	    found = i;
+	    break;
+	}
+    }
+
+    if(exec_args[0] == NULL) {
+	exit(0);
+    }
+
+
     if (strcmp(exec_args[0], "cd") == 0) {
         exec_cd(exec_args);
     } else if (strcmp(exec_args[0], "path") == 0) {
@@ -108,16 +114,16 @@ void exec(char **exec_args) {
     } else if (strcmp(exec_args[0], "exit") == 0) {
         exec_exit(exec_args);
     } else {
+	if (found == MAX_PATH) {
+	    print_error();
+	    exit(0);
+	}
         int rc = fork();
         if (rc < 0) {
 	    print_error();
 	    exit(1);
         } else if (rc == 0) {
-            if (access(PATH[0], X_OK) == 0) { 
-		exec_cmd(PATH, exec_args);
-	    } else {
-		print_error();
-	    } 
+            exec_cmd(PATH, exec_args);
         } else {
 	    wait(NULL);
         }

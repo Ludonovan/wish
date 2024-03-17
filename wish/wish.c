3ba0411aecd598bd3fcb09746af78a34e8ab0e26
Ludonovan <lucasdonovan@live.com>
2024-03-17 18:59:46 -0400

    14 ok, 15 & new1 bad

--- wish/wish.c
+++ wish/wish.c
@@ -11,11 +11,16 @@ char error_message[30] = "An error has occurred\n";
 
 int main(int argc, char *argv[]) {
     lineNum = 1;
-    char *filename = argv[1];
+
     if (argc < 2) {
          interactive();
-    } else if (argc == 2) {	
-        batch(filename);
+    } else if (argc == 2) {
+        if (access(argv[1], F_OK) == 0) {
+            batch(argv[1]);
+        } else {
+            print_error();
+            exit(1);
+        }
     } else {
         print_error();
         exit(1);
